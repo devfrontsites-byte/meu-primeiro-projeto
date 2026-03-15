e786eb84a604abc013aab8ab67a699d1b07c031b
devfrontsites-byte <devfrontsites@gmail.com>
2026-03-15 14:18:05 -0300

    meu priemiro projeto

---
+++ scripts.js
@@ -0,0 +1,28 @@
+
+const form = document.querySelector(".Formulario-fale-conosco")
+const mascara = document.querySelector(".mascara-formulario")
+
+
+
+
+function cliquei(){
+
+form.style.left = "50%"
+form.style.transform = "translatex(-50%)"
+mascara.style.visibility = "visible"
+}
+function sumir(){
+form.style.left = "-300px"
+form.style.transform = "translatex(0)"
+mascara.style.visibility = "hidden"
+}
+
+function orcamento(){
+form.style.left = "50%"
+form.style.transform = "translatex(-50%)"
+mascara.style.visibility = "visible"
+}
+
+/*     Top: 30%;
+        left: -300px;     */ 
+
